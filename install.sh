0204f2c1653f0116e08222901cb4a39eabe6a23e
Leon Collazo Arechaga <47929977+p0zzle@users.noreply.github.com>
2022-02-17 13:22:56 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Setup de servidor VNC, Minty.
+# Setup de servidor VNC, Instalacion de Impresoras, modificacion de IP, etc, etc. El punto es dejar una maquina con Minty completamente utilizable.
+# Por Leon Collazo Arechaga; p0zzel
+# V0.1
 
+# Script start;
 # Borrando vino e instalando x11vnc
 
 cd ~
@@ -19,7 +22,9 @@ sudo x11vnc --storepasswd /etc/x11vnc/vncpwd
 
 sudo xed /lib/systemd/system/x11vnc.service
 
-echo "[Unit]\n Description=Start x11vnc at startup.\n After=multi-user.target\n\n [Service]\n Type=simple\n ExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth /etc/x11vnc/vncpwd -rfbport 5900 -shared -noxkb -nomodtweak \n [Install]\n WantedBy=multi-user.target\n\n" >> /lib/systemd/system/x11vnc.service
+printf "[Unit]\nDescription=Start x11vnc at startup.\nAfter=multi-user.target\n\n[Service]\n
+Type=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth /etc/x11vnc/vncpwd 
+-rfbport 5900 -shared -noxkb -nomodtweak\n\n[Install]\nWantedBy=multi-user.target\n\n" >> /lib/systemd/system/x11vnc.service
 
 # Recarga los servicios
 
@@ -32,3 +37,23 @@ sudo systemctl enable x11vnc.service
 # Empieza el servicio para evitar reiniciarlo
 
 sudo systemctl start x11vnc.service
+
+# Instalacion de impresoras Hasar, sourced de Mario Palacios, V2.
+# Instalacion de aplicaciones y librerias necesarias.
+sudo apt-get install cups ia32-libs libcups2:i368 libcupsfilters1:i386 libcupsimage2:i386 -y
+
+# Las Hasar 250 pueden llegar a no instalarse correctamente el driver, hay que copiar las bibliotecas de las mismas.
+
+sudo cp /usr/lib/x86_64-linux-gnu/libcups.so.2 /usr/lib/x86_64-linux-gnu/libcups.so
+sudo cp /usr/lib/x86_64-linux-gnu/libcupsimage.so.2 /usr/lib/x86_64-linux-gnu/libcupsimage.so
+sudo cp /usr/lib/i386-linux-gnu/libcups.so.2 /usr/lib/i386-linux-gnu/libcups.so
+sudo cp /usr/lib/i386-linux-gnu/libcupsimage.so.2 /usr/lib/i386-linux-gnu/libcupsimage.so
+
+cd 250
+sudo ./setup
+cd ..
+cd 1000
+sudo ./setup
+cd ..
+
+printf "La instalacion de las impresoras termino"
