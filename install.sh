bd49fda6d8d36eb199d7b5d84eaf7ecb6b9df24b
Leon Collazo Arechaga <47929977+p0zzle@users.noreply.github.com>
2022-02-22 11:21:06 -0300

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
-# Setup de servidor VNC, Instalacion de Impresoras, modificacion de IP, etc, etc. El punto es dejar una maquina con Minty completamente utilizable.
+# Setup de servidor VNC, Instalacion de Impresoras,
+# modificacion de IP, etc, etc.
+# El punto es dejar una maquina con Minty completamente utilizable.
 
 # Autor: Leon Collazo Arechaga
 # Github: https://github.com/p0zzle
 # Fecha: 17/02/2022
+# Ultima actualizacion: 22/02/2022
+# experimental-install-V1.0.0
 
-# V0.0.2
-# Algunas Variables.
+
 # Script start;
 # Mensaje de bienvenida y preguntas para completar algunos temas basicos
 printf "Hola!Necesito que me des un poco de informacion sobre la instalacion.\n"
-#printf "Te dejo las opciones numericas para hacerlo relativamente mas facil\n"
 printf "Primero, donde estas instalandolo?\n0 - Farmatotal - 0\n101 - Centro - 101\n102 - Alem - 102\n103 - Perito - 103\n104 - Kuanip - 104\n105 - Tolhuin - 105\n106 - Farmatotal - 106\n"
 read -p "Escribi el numero de la sucursal == " suc
-
 printf "Segundo, cual es el hostname de esta PC?"
 read -p " Hostname == " hostn
-# Borrando vino, instalando x11 y iptux
 
+# Borrando vino, instalando x11 y iptux
+sudo apt-get update
 sudo apt-get -y remove vino
 sudo apt-get install -y x11vnc iptux chromium
 
@@ -31,7 +33,6 @@ sudo mkdir /media/t
 
 # Creo el servicios de x11vnc para systemd
 
-
 printf "[Unit]
 Description=Start x11vnc at startup.
 After=multi-user.target\n
@@ -43,20 +44,21 @@ RestartSec=5s\n
 [Install]
 WantedBy=multi-user.target" > /lib/systemd/system/x11vnc.service
 
-
 # Reinicio y habilito x11vnc
+
 sudo systemctl daemon-reload
 sudo systemctl enable x11vnc.service
 sudo systemctl start x11vnc.service
 
 # Cambio de Hostname, insercion de IP en base a la eleccion original.
+
 echo "$hostn" > /etc/hostname
-# sed en caso de que iptux solucione (o yo entienda) como se crean las configuraciones iniciales.
-# sed -i -e "s/\"nick_name\" : \"hostname\"/\"nick_name\" : \"$hostn\"/g" /home/tys/.iptux/config.json
 echo "//192.168.$suc.5/factura/ /media/x      cifs    username=tys,password=tys,file_mode=0666,dir_mode=0777" >> /etc/fstab
 printf "\n" >> /etc/fstab
 echo "//192.168.$suc.5/temporal /media/t      cifs    username=tys,password=tys,file_mode=0666,dir_mode=0777" >> /etc/fstab
+
 ########## INICIO DE LA CONFIGURACION DE IPTUX.
+
 printf '{
 	"archive_path" : "/home/tys",
 	"belong_group" : "",
@@ -95,14 +97,23 @@ printf '\n	"open_blacklist" : false,
 	"version" : 1,
 	"volume_degree" : 0
 }' >> /home/tys/.iptux/config.json
-########## FINAL DE LA CONFIGURACION DE IPTUX
 
+printf "[Desktop Entry]
+Type=Application
+Exec=iptux
+Hidden=False
+X-GNOME-Autostart-enabled=true
+Name=iptux
+Comment=LAN Messaging App" > /home/tys/.config/autostart
+
+########## FINAL DE LA CONFIGURACION DE IPTUX
 
 # Instalacion de impresoras Hasar, sourced de Mario Palacios, V2.
 # Instalacion de aplicaciones y librerias necesarias.
 # Paquetes que dan error! libcups2:i368
 # libcups2:i368 no puede ser localizado
 # libcupsfilters1:i386 no tiene un candidato para la instalacion
+# Posibles paquetes; libcups2, libcupsfilters1.
 
 sudo apt install cups ia32-libs libcupsimage2:i386 -y
 
@@ -120,9 +131,12 @@ cd ..
 cd 1000
 sudo ./setup
 cd ..
-# Finalmente montamos la particion
+
+# Finalmente montamos las particiones
 
 sudo mount -a
 
 printf "La instalacion termino! ※ (^o^)/※\n"
-printf "Seria una buena idea reiniciar la computadora."
+printf "Se va a lanzar Gnome Session Properties. Despues habria que reiniciar."
+
+gnome-session-properties
--- simple-install.sh
+++ simple-install.sh
@@ -1,49 +1,91 @@
 #!/bin/bash
 
-# Setup de servidor VNC, Instalacion de Impresoras, modificacion de IP, etc, etc. El punto es dejar una maquina con Minty completamente utilizable.
+# Setup de servidor VNC, Instalacion de Impresoras,
+# modificacion de IP, etc, etc.
+# El punto es dejar una maquina con Minty completamente utilizable.
+
 
 # Autor: Leon Collazo Arechaga
 # Github: https://github.com/p0zzle
 # Fecha: 17/02/2022
+# Ultima actualizacion: 22/02/2022
+# simple-install-V1.0.0
 
-# V0.0.2
-# Algunas Variables.
+
 # Script start;
-# Corro instalacion/desinstalacion antes de el texto para hacer todo mas streamline
-sudo apt-get -y remove vino
-sudo apt-get install -y x11vnc iptux chromium
 # Mensaje de bienvenida y preguntas para completar algunos temas basicos
+
 printf "Hola!Necesito que me des un poco de informacion sobre la instalacion.\n"
-# Sucursales
 printf "Primero, donde estas instalandolo?\n0 - Farmatotal - 0\n101 - Centro - 101\n102 - Alem - 102\n103 - Perito - 103\n104 - Kuanip - 104\n105 - Tolhuin - 105\n106 - Farmatotal - 106\n"
 read -p "Escribi el numero de la sucursal == " suc
-# Hostname
 printf "Segundo, cual es el hostname de esta PC?"
 read -p " Hostname == " hostn
-# Creo la carpeta y genero el password
+
+# Borrando vino, instalando x11 y iptux
+sudo apt-get update
+sudo apt-get -y remove vino
+sudo apt-get install -y x11vnc chromium
+
+# Creando las carpetas, generando password de x11.
+
 sudo mkdir /etc/x11vnc
 sudo x11vnc --storepasswd /etc/x11vnc/vncpwd
-# Creando las carpetas, generando password de x11.
 sudo mkdir /media/x
 sudo mkdir /media/t
+
 # Creo el servicios de x11vnc para systemd
-printf "[Unit]\nDescription=Start x11vnc at startup.\nAfter=multi-user.target\n\n[Service]\n
-Type=simple\nExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth\n /etc/x11vnc/vncpwd
--rfbport 5900 -shared -noxkb -nomodtweak\n\n[Install]\nWantedBy=multi-user.target\n\n" > /lib/systemd/system/x11vnc.service
+
+printf "[Unit]
+Description=Start x11vnc at startup.
+After=multi-user.target\n
+[Service]
+Type=simple
+ExecStart=/usr/bin/x11vnc -auth guess -forever -noxdamage -repeat -rfbauth /etc/x11vnc/vncpwd -rfbport 5900 -shared -noxkb -nomodtweak
+Restart=on-failure
+RestartSec=5s\n
+[Install]
+WantedBy=multi-user.target" > /lib/systemd/system/x11vnc.service
+
 # Reinicio y habilito x11vnc
+
 sudo systemctl daemon-reload
 sudo systemctl enable x11vnc.service
 sudo systemctl start x11vnc.service
 
 # Cambio de Hostname, insercion de IP en base a la eleccion original.
+
 echo "$hostn" > /etc/hostname
-# sed en caso de que iptux solucione (o yo entienda) como se crean las configuraciones iniciales.
-# sed -i -e "s/\"nick_name\" : \"hostname\"/\"nick_name\" : \"$hostn\"/g" /home/tys/.iptux/config.json
 echo "//192.168.$suc.5/factura/ /media/x      cifs    username=tys,password=tys,file_mode=0666,dir_mode=0777" >> /etc/fstab
 printf "\n" >> /etc/fstab
 echo "//192.168.$suc.5/temporal /media/t      cifs    username=tys,password=tys,file_mode=0666,dir_mode=0777" >> /etc/fstab
 
+# Instalacion de impresoras Hasar, sourced de Mario Palacios, V2.
+# Instalacion de aplicaciones y librerias necesarias.
+# Paquetes que dan error! libcups2:i368
+# libcups2:i368 no puede ser localizado
+# libcupsfilters1:i386 no tiene un candidato para la instalacion
+# Posibles paquetes; libcups2, libcupsfilters1.
+
+sudo apt install cups ia32-libs libcupsimage2:i386 -y
+
+# Las Hasar 250 pueden llegar a no instalarse correctamente el driver, hay que copiar las bibliotecas de las mismas.
+
+sudo cp /usr/lib/x86_64-linux-gnu/libcups.so.2 /usr/lib/x86_64-linux-gnu/libcups.so
+sudo cp /usr/lib/x86_64-linux-gnu/libcupsimage.so.2 /usr/lib/x86_64-linux-gnu/libcupsimage.so
+sudo cp /usr/lib/i386-linux-gnu/libcups.so.2 /usr/lib/i386-linux-gnu/libcups.so
+sudo cp /usr/lib/i386-linux-gnu/libcupsimage.so.2 /usr/lib/i386-linux-gnu/libcupsimage.so
+
+tar -xzvf hasar.tar.gz
+cd 250
+sudo ./setup
+cd ..
+cd 1000
+sudo ./setup
+cd ..
+
+# Finalmente montamos las particiones
+
 sudo mount -a
 
 printf "La instalacion termino! ※ (^o^)/※\n"
-printf "Seria una buena idea reiniciar la computadora para aplicar algunos cambios."
+printf "Seria una buena idea reiniciar la computadora."
